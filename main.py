2c7b76da3381d33a7c46fc15bcd1294976620839
Kraktun <kraktun.dev@gmail.com>
2020-12-23 10:09:59 +0100

    revised p1

--- main.py
+++ main.py
@@ -1,38 +1,36 @@
 from prover import *
 from utils import *
-import time
+from timeit import default_timer as timer
 import random
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
-def main():
+def alg_complexity():
 	
-	lc_values = [4, 8, 16, 20, 24, 28, 30, 32, 64, 128]
-	lk_values = [4, 8, 16, 20, 24, 28, 30, 32, 64, 128]
-	for c in lc_values:
-		lc = c
-		avg_time = []
-		for k in tqdm(lk_values):
-			p = Prover(counter_v=0)
-			lk = k
-			p.compute_key(lk=lk, isAttacker=False)
-			avg = 0
-			for i in range(10**4):
-				start = time.time()
-				c, n = p.send_welcome(lc=lc) # generate challenge and n
-				p.compute_u3() # compute r, r1 from c and n and verify that r = r1
-				end = time.time()
-				avg += (end - start)
-			avg = avg/10**4
-			avg_time.append(avg)
-		plt.plot(lc_values, avg_time)
-	#plt.yscale('log')
-	plt.xlabel('key length')
-	plt.ylabel('average elapsed time')
-	plt.legend(lc_values)
-	plt.show()
+    lc_values = [4, 8, 16, 20, 24, 28, 30, 32, 64, 128]
+    lk_values = [4, 8, 16, 20, 24, 28, 30, 32, 64, 128]
+    fig = plt.figure()
+    for lc in tqdm(lc_values):
+        avg_time = []
+        for lk in lk_values:
+            p = Prover(counter_v=0)
+            p.compute_key(lk=lk, isAttacker=False)
+            repetitions = 10**4
+            start = timer()
+            for i in range(repetitions):
+                c, n = p.send_welcome(lc=lc) # generate challenge and n
+                p.compute_u3() # compute r, r1 from c and n and verify that r = r1
+            end = timer()
+            avg = (end-start)/repetitions
+            avg_time.append(avg)
+        plt.plot(lc_values, avg_time)
+    plt.legend([ f"lc = {x}" for x in lc_values])
+    plt.title('Algorithm complexity')
+    plt.xlabel('key length')
+    plt.ylabel('average elapsed time')
+    plt.show(block = False)
 	
-	'''
+def attack_observer():
 	# ATTACK WITH OBSERVED PREVIOUS ROUND
 	lc_values = [4, 8, 16, 20, 24, 28, 30, 32, 64, 128]
 	lk_values = [4, 8, 16, 20, 24, 28, 30, 32, 64, 128]
@@ -66,7 +64,7 @@ def main():
 	plt.legend(lc_values)
 	plt.show()
 	
-	
+def attack_brute():
 	# ATTACK WITH OBSERVED PREVIOUS ROUND
 	lc_values = [4, 8, 16, 20, 24, 28, 30, 32, 64, 128]
 	lk_values = [4, 8, 16, 20, 24, 28, 30, 32, 64, 128]
@@ -99,9 +97,7 @@ def main():
 	plt.ylabel('average elapsed time')
 	plt.legend(lc_values)
 	plt.show()
-    '''
-
-    
 
 if __name__ == "__main__": 
-    main()
+    alg_complexity()
+    plt.show()
